88b04e19c0ee2cafc6b122907bacbcc8081b96aa
Steve Stone <steve@digitalstache.com>
2012-11-21 13:20:32 -0500

    - GUI modifications - segments table should be scalable

--- app/models/segment.js
+++ app/models/segment.js
@@ -7,6 +7,7 @@ var SegmentSchema = new Schema({
     segment_title: {type : String, default : ''}
   , segment_trt: {type : String, default : ''}
   , segment_type: {type : String, default : ''}
+  , order: {type : Number, default : 0}
 })
 
 mongoose.model('Segment', SegmentSchema)
