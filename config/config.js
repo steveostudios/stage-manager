c1a774a5c19c8591aafd92253a4ca026ff1891df
Steve Stone <steve@digitalstache.com>
2013-04-12 08:54:51 -0400

    - Starting over again!

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
       app: {
         name: 'Nodejs Express Mongoose Demo'
       },
-      db: 'mongodb://heroku_app14650257:9n1sp5gvflhsm9e39nu8648tkb@ds053877.mongolab.com:53877/heroku_app14650257',
+      db: 'mongodb://localhost/noobjs_dev',
       facebook: {
           clientID: "APP_ID"
         , clientSecret: "APP_SECRET"
