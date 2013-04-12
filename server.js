2e90e07889e79aab7819a019ce33c886fd7753b4
Steve Stone <steve@digitalstache.com>
2013-04-12 08:56:52 -0400

    - Starting over again!

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ require('./config/express')(app, config, passport)
 require('./config/routes')(app, passport, auth)
 
 // Start the app by listening on <port>
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 5000
 app.listen(port)
 console.log('Express app started on port '+port)
