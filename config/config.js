bcfe502165dcae491b14381f58cbaaec1f95ff0b
Steve Stone <steve@digitalstache.com>
2013-04-12 08:03:52 -0400

    Starting Over

---
+++ config/config.js
@@ -0,0 +1,36 @@
+
+module.exports = {
+    development: {
+      root: require('path').normalize(__dirname + '/..'),
+      app: {
+        name: 'Nodejs Express Mongoose Demo'
+      },
+      db: 'mongodb://heroku_app14650257:9n1sp5gvflhsm9e39nu8648tkb@ds053877.mongolab.com:53877/heroku_app14650257'
+      facebook: {
+          clientID: "APP_ID"
+        , clientSecret: "APP_SECRET"
+        , callbackURL: "http://localhost:3000/auth/facebook/callback"
+      },
+      twitter: {
+          clientID: "CONSUMER_KEY"
+        , clientSecret: "CONSUMER_SECRET"
+        , callbackURL: "http://localhost:3000/auth/twitter/callback"
+      },
+      github: {
+          clientID: 'APP_ID'
+        , clientSecret: 'APP_SECRET'
+        , callbackURL: 'http://localhost:3000/auth/github/callback'
+      },
+      google: {
+          clientID: "APP_ID"
+        , clientSecret: "APP_SECRET"
+        , callbackURL: "http://localhost:3000/auth/google/callback"
+      }
+    }
+  , test: {
+
+    }
+  , production: {
+
+    }
+}
